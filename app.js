279eafb3260989fb32153e03a8c0130aeab475c6
bmorck <bmorckpro@gmail.com>
2023-02-01 13:42:30 -0700

    New REST API in app.js and link in index.html

--- app.js
+++ app.js
@@ -25,6 +25,21 @@ app.get('/home', (req, res) => {
     })
 })
 
+app.get('/Display', (req, res) => {
+    res.writeHead(200); // careful -  cant send response code twice!
+    // res.send(200, 'This is a placeholder for the landing page of my website.');
+    // res.writeHead(200, { 'Content-type': 'text/html' })
+    // fs.readFile('index.html', function(error, data) {
+    //     if(error){
+    //         res.writeHead(404)
+    //         res.write('Error: File Not Found')
+    //     } else{
+    //         res.write(data)
+    //     }
+    //     res.end()
+    // })
+})
+
 const port = process.env.PORT || 4000
 //Listen on port 5000
 app.listen(port, () => console.log(`Listening on port ${port}...`));
